f7d262ddd9a1d07613c362416a339710aa24362a
Omar V <osmarvillagran@gmail.com>
2025-09-30 02:14:40 -0300

    Solucion final: Subir todos los archivos ejectados y la corrección de webpack.config.js (compact: false)

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Credenciales de tu proyecto 'mi-app-de-tareas-b51c0'
 const firebaseConfig = {
   apiKey: "AIzaSyCkpV7WNbt0SGZyJKJBCIVwaiSSudgKnYs",
   authDomain: "mi-app-de-tareas-b51c0.firebaseapp.com",
@@ -11,10 +10,8 @@ const firebaseConfig = {
   appId: "1:441650271595:web:4e5d01e0ca410b55d1282e"
 };
 
-// Inicializa la aplicación de Firebase
 const app = initializeApp(firebaseConfig);
 
-// Inicializa y exporta la instancia de Firestore para usarla en los componentes
 const db = getFirestore(app);
 
 export { db };
